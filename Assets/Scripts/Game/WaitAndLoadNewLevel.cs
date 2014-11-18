ee57d5656b98bf48eecbc0d60fcd837fa9ba90e4
Antonio Vildes Barbosa <antoniovildes@joystreet.com.br>
2014-11-17 21:25:00 -0300

    Analytics v3

---
+++ Assets/Scripts/Game/WaitAndLoadNewLevel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaitAndLoadNewLevel : MonoBehaviour
+{
+	public string level;
+
+	public float seconds;
+
+	IEnumerator WaitAndLoad ()
+	{
+		yield return new WaitForSeconds(seconds);
+		Application.LoadLevel(level);
+	}
+
+	void Start ()
+	{
+		StartCoroutine(WaitAndLoad());
+	}
+}
